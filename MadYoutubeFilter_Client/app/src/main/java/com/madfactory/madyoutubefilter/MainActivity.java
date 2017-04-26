98f8cbbf9c393125aa405420275a8911407f0876
NEXON\nnnyyy <nnnyyy@nate.com>
2017-04-26 20:03:58 +0900

    뼈대 만드는 중

--- MadYoutubeFilter_Client/app/src/main/java/com/madfactory/madyoutubefilter/MainActivity.java
+++ MadYoutubeFilter_Client/app/src/main/java/com/madfactory/madyoutubefilter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.madfactory.madyoutubefilter;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,11 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +27,22 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         viewPager = (ViewPager)findViewById(R.id.viewpager);
+        SetupViewPager(viewPager);
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
 
+    private void SetupViewPager(ViewPager vp) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addItem("Sports" , new SportFragment());
+        adapter.addItem("Game" , new GameFragment());
+        vp.setAdapter(adapter);
     }
 }
 
 class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<PagerItem> mItemList = new ArrayList<>();
 
     public ViewPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -34,16 +50,30 @@ class ViewPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return mItemList.get(position).fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mItemList.size();
+    }
+
+    public void addItem(String _sTitle, Fragment _fragment) {
+        mItemList.add(new PagerItem(_fragment, _sTitle));
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mItemList.get(position).sTitle;
     }
 }
 
 
 class PagerItem {
-
+    PagerItem(Fragment _f, String _sTitle) {
+        fragment = _f;
+        sTitle = _sTitle;
+    }
+    public Fragment fragment;
+    public String sTitle;
 }
