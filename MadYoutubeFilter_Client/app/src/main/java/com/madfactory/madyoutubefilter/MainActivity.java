635c14c3cb78288da472aa6b4849aba6d37fffff
NEXON\nnnyyy <nnnyyy@nate.com>
2017-04-26 15:36:39 +0900

    위대한 첫 커밋

---
+++ MadYoutubeFilter_Client/app/src/main/java/com/madfactory/madyoutubefilter/MainActivity.java
@@ -0,0 +1,49 @@
+package com.madfactory.madyoutubefilter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+
+    }
+}
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
+
+
+class PagerItem {
+
+}
